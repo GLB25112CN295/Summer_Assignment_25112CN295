117e596d297af73ac2b3a5c520654e86674bd5a8
GLB25112CN295 <vaibhavmishrafzd87@gmail.com>
2026-05-30 07:25:58 +0530

    Create Q1.c

---
+++ Day_3/Q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n,i,flag=0;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=2;i<=n-1;i++){
+        if(n%i==0){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    printf("prime number ");
+    else
+    printf("not prime");
+    return 0;
+}
