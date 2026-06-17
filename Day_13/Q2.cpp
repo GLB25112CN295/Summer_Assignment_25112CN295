9f57863ff6f61e92f836cef1b2251174209e9230
GLB25112CN295 <vaibhavmishrafzd87@gmail.com>
2026-06-17 17:18:29 +0530

    Create Q2.cpp

---
+++ Day_13/Q2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[1000];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum= sum+arr[i];
+    }
+    cout<<"the sum of number is "<<sum<<endl;
+    int avg;
+    avg=sum/n;
+    cout<<"the avg is "<<avg;
+}
