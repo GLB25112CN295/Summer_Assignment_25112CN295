73ac932da22e09ca0a8b58139852fa53ca05f860
GLB25112CN295 <vaibhavmishrafzd87@gmail.com>
2026-06-17 20:29:15 +0530

    Create Q1.cpp

---
+++ Day_14/Q1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int key;
+    cin>>key;
+    for(int i=0;i<n;i++){
+        if(arr[i]==key);
+    }
+    cout<<"element is found"<<endl;
+}
