331c9e64480ef46102495a044fb37aff11a3f65d
GLB25112CN295 <vaibhavmishrafzd87@gmail.com>
2026-06-25 21:21:24 +0530

    Create Q4.cpp

---
+++ Day_17/Q4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr1[100];
+    for(int i=0;i<n;i++){
+        cin>>arr1[i];
+    }
+    int m;
+    cin>>m;
+    int arr2[100];
+    for(int i=0;i<m;i++){
+        cin>>arr2[i];
+    }
+    for(int i=0;i<n;i++ ){
+        for(int j=0;j<m;j++){
+            if(arr1[i]==arr2[j]){
+                cout<<arr1[i]<<" ";
+            }
+
+        }
+    }
+
+}
