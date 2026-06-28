49d35d54298bf2511ae142f734688c62256c86e0
GLB25112CN295 <vaibhavmishrafzd87@gmail.com>
2026-06-28 19:55:06 +0530

    Create Q4.cpp

---
+++ Day_18/Q4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 5, 8, 20, 15};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    // Sorting in descending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+    cout << "Array in descending order: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
